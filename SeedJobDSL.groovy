c31245518cf5cc45889c30850dd0dd9ff6261121
Adeoye Oyewole <adeoye@myyahoo.com>
2024-10-26 13:38:56 -0400

    update terraform code

--- SeedJobDSL.groovy
+++ SeedJobDSL.groovy
@@ -11,7 +11,7 @@ projects.each { project ->
     def repoUrl = project.repo
     def branch = project.branch
     def jenkinsfilePath = project.jenkinsfile
-    def credentialsId = 'git-creds'
+    def credentialsId = 'github_creds'
 
     // Create pipeline job
     pipelineJob(pipelineName) {
